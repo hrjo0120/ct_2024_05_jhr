a24844bebc6e693916be8189ca97c4adaa2f6c8d
hrjo0120 <hrjo0120@gmail.com>
2024-07-02 09:20:33 +0900

    0702

--- src/main/java/com/ki/level0/p120851/Main.java
+++ src/main/java/com/ki/level0/p120851/Main.java
@@ -1,16 +1,16 @@
 package com.ki.level0.p120851;
-
+// 숨어있는 숫자의 덧셈 (1)
 public class Main {
 }
-// 숨어있는 숫자의 덧셈
+
 class Solution {
     public int solution(String my_string) {
+        my_string = my_string.replaceAll("[a-zA-Z]", "");
         String[] arr = my_string.split("");
         int answer = 0;
-        for(int i = 0; i < arr.length; i++) {
-           answer += Integer.parseInt(arr[i]);
+        for(String str : arr) {
+            answer += Integer.parseInt(str);
         }
-
         return answer;
     }
 }
